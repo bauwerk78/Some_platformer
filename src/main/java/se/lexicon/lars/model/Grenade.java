ab52e456a51b74f09532b5242fcf24565f33a215
bauwerk78 <57097401+bauwerk78@users.noreply.github.com>
2020-01-22 22:01:37 +0100

    Mostly working with bounces of grenades. Made a new function that will most likely solve plenty of things hopefully.

--- src/main/java/se/lexicon/lars/model/Grenade.java
+++ src/main/java/se/lexicon/lars/model/Grenade.java
@@ -6,6 +6,8 @@ import javafx.scene.image.Image;
 import se.lexicon.lars.implementer.MainGame;
 import se.lexicon.lars.tools.Delayer;
 
+import java.util.Arrays;
+
 public class Grenade extends GameObject {
 
     private final double fakeDeltaTime = 0.015;
@@ -23,6 +25,7 @@ public class Grenade extends GameObject {
     private boolean renderExplosion;
     private boolean done;
     private boolean grounded;
+    private Object[][] collideAbles = new Object[4][2];
 
     public Grenade(double posX, double posY) {
         super(posX, posY);
@@ -45,20 +48,37 @@ public class Grenade extends GameObject {
 
     }
 
+    private void checkCollideAbles(MainGame mg) {
+        //Left
+        collideAbles[0][0] = mg.getCollisions((getPositionX()) / (Level.TILESIZE) - 1, (getPositionY() + getObjectHeight()) / Level.TILESIZE);
+        collideAbles[0][1] = Math.ceil((getPositionX() + Level.TILESIZE) / (Level.TILESIZE) - 1) * (Level.TILESIZE);
+        //System.out.println((double)collideAbles[0][1]);
+        //Right
+        collideAbles[1][0] = mg.getCollisions((getPositionX() / Level.TILESIZE) + 1, (getPositionY() + getObjectHeight()) / Level.TILESIZE);
+        collideAbles[1][1] = Math.floor((getPositionX() + getObjectWidth()) / (Level.TILESIZE) + 1) * (Level.TILESIZE);
+        //System.out.println((double)collideAbles[1][1]);
+        //Up
+        collideAbles[2][0] = mg.getCollisions((getPositionX() / Level.TILESIZE), (getPositionY() + getObjectHeight()) / Level.TILESIZE - 1);
+        //Down
+        collideAbles[3][0] = mg.getCollisions((getPositionX() / Level.TILESIZE), (getPositionY() + getObjectHeight()) / Level.TILESIZE + 1);
+        collideAbles[3][1] = Math.floor((getPositionY()) / (Level.TILESIZE) + 1) * (Level.TILESIZE);
+        //System.out.println((double) collideAbles[3][1]);
+    }
+
     @Override
     protected void update(Scene scene, MainGame mg) {
 
 
-        //TODO perhaps initialize speedx as either positive or negative?!?!
-        //TODO might have to do a "full" else extra here on both right and left to get movement if there is no else in this statements.
-        //Collision detection.
+        //Collision detection begins.
+        checkCollideAbles(mg);
+
         if (goingRight) {
             if (mg.getCollisions((getPositionX() + getObjectWidth()) / (Level.TILESIZE), (getPositionY() + getObjectHeight()) / Level.TILESIZE)) {
                 //System.out.println("collidedposx going right : " + (mg.getCollidedPosX() - 1));
                 //System.out.println("going right triggered!");
-
-                goingRight = false;
-
+                if (!(boolean) collideAbles[3][0]) {
+                    goingRight = false;
+                }
             } else {
                 setPositionX(getPositionX() + (getObjectSpeedX() * fakeDeltaTime));
                 //setPositionY(getPositionY() + (getObjectSpeedY() * fakeDeltaTime));
@@ -69,9 +89,9 @@ public class Grenade extends GameObject {
         if (!goingRight) {
             if (mg.getCollisions((getPositionX() / Level.TILESIZE), (getPositionY() + getObjectHeight()) / Level.TILESIZE)) {
                 //System.out.println("going left triggered!");
-
-                goingRight = true;
-
+                if (!(boolean) collideAbles[3][0]) {
+                    goingRight = true;
+                }
             } else {
                 setPositionX(getPositionX() - (getObjectSpeedX() * fakeDeltaTime));
                 //setPositionY(getPositionY() + (getObjectSpeedY() * fakeDeltaTime));
@@ -82,32 +102,23 @@ public class Grenade extends GameObject {
             if ((mg.getCollisions(Math.floor(getPositionX() + getObjectWidth()) / Level.TILESIZE, Math.floor(getPositionY() + getObjectHeight()) / (Level.TILESIZE))
                     || (mg.getCollisions(Math.ceil(getPositionX() + getObjectWidth()) / Level.TILESIZE, Math.floor(getPositionY() + getObjectHeight()) / (Level.TILESIZE))))) {
 
-                //grounded = true;
-/*                if(getPositionY() + getObjectHeight() > Math.floor(getPositionY() + getObjectHeight()) / (Level.TILESIZE) * Level.TILESIZE) {
-                    setPositionY(getPositionY() - 2);
-                    grounded = true;
-                    setObjectSpeedY(0);
-                }*/
                 //Set half of both Y and X speed when bouncing on floor.
                 if (getObjectSpeedY() > 0) {
                     setObjectSpeedY(-(getObjectSpeedY() / 2));
-                    System.out.println("speedY: " + Math.abs(getObjectSpeedY()));
+                    //System.out.println("speedY: " + Math.abs(getObjectSpeedY()));
                 }
-                //TODO ????
                 if (getObjectSpeedX() > 0) {
                     setObjectSpeedX((getObjectSpeedX() / 2));
                     //System.out.println("speedX: " + Math.abs(getObjectSpeedX()));
-                } else {
-                    setObjectSpeedX((getObjectSpeedX() / 2));
                 }
 
             } else {
                 setPositionY(getPositionY() + (getObjectSpeedY() * fakeDeltaTime));
             }
-        } else {
+        } /*else {
             setPositionY(getPositionY() + (getObjectSpeedY() * fakeDeltaTime));
         }
-
+*/
         //Colliding up
         if (getObjectSpeedY() < 0) {
             if ((mg.getCollisions(Math.floor(getPositionX()) / Level.TILESIZE, Math.floor(getPositionY()) / (Level.TILESIZE))
@@ -115,8 +126,11 @@ public class Grenade extends GameObject {
                 //System.out.println("colliding up");
                 //System.out.println(Math.abs(getObjectSpeedY()));
                 setObjectSpeedY(Math.abs(getObjectSpeedY()));
+            } else {
+                setPositionY(getPositionY() + (getObjectSpeedY() * fakeDeltaTime));
             }
         }
+
         //Explosion
         if (!exploded) {
             exploded = delayer.delayTimer(3);
